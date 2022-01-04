77c1552bb381aefb4c87fadfd9982e41ea82884d
SKupisz <szym-ku@wp.pl>
2022-01-04 19:46:45 +0100

    Added Schema.org to the project to improve the SEO

--- src/components/headTag.jsx
+++ src/components/headTag.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { Person } from "schema-dts";
+import { helmetJsonLdProp } from "react-schemaorg";
 
 const HeadTag = ({siteName, description="Simon George Kupisz's site", robotsTags="all", keywords=""}) => {
-    return <Helmet>
+    return <Helmet script = {[
+        helmetJsonLdProp<Person>({
+            "@context": "https://schema.org",
+            "@type": "Person",
+            "name": "Simon G. Kupisz",
+            "alternateName": ["Szymon Kupisz", "Simon Kupisz"],
+            "disambiguatingDescription": "A Polish software developer",
+            "knowsAbout": ["Programming", "Web apps development", "Algorithms"],
+            "jobTitle": "Programmer",
+            "url": "https://skupisz.netlify.app",
+            "birthDate": "2003-09-16",
+            "birthPlace": "Warsaw, Poland",
+            "memberOf": "https://hackclub.pl",
+            "nationality": "Poland",
+            "description": "Since he can remember, Simon is a big passionate of programming.",
+            "familyName": "Kupisz",
+            "seeks": ["knowledge","fame"]
+        })
+    ]}>
         <title>{siteName}</title>
         <meta name="description" content={description}/>
         <meta name="robots" content={robotsTags}/>
