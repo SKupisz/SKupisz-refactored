b96d0ac889437c3760c4c2bb48e7170770b70fd6
SKupisz <szym-ku@wp.pl>
2022-01-11 12:18:08 +0100

    Fixing and improving structured data #2

--- src/components/headTag.jsx
+++ src/components/headTag.jsx
@@ -1,33 +1,9 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { Person } from "schema-dts";
-import { helmetJsonLdProp } from "react-schemaorg";
 import AuthorImage from "../assets/simonKupisz.jpg";
 
 const HeadTag = ({siteName, description="Simon George Kupisz's site", robotsTags="all", keywords=""}) => {
-    return <Helmet script={[
-        helmetJsonLdProp<Person>({
-            "@context": "https://schema.org",
-            "@type": "Person",
-            "name": "Simon G. Kupisz",
-            "alternateName": ["Szymon Kupisz", "Simon Kupisz"],
-            "disambiguatingDescription": "A Polish software developer",
-            "knowsAbout": ["Programming", "Web apps development", "Algorithms"],
-            "jobTitle": "Programmer",
-            "url": "https://skupisz.netlify.app",
-            "birthDate": "2003-09-16",
-            "birthPlace": "Warsaw, Poland",
-            "memberOf": "https://hackclub.pl",
-            "nationality": "Poland",
-            "description": "Since he can remember, Simon is a big passionate of programming.",
-            "familyName": "Kupisz",
-            "seeks": ["knowledge","fame"],
-            "gender": "male",
-            "givenName": ["Simon", "Szymon"],
-            "knowsLanguage": ["English", "Polish"],
-            "image": AuthorImage
-        }),
-    ]}>
+    return <Helmet>
         <title>{siteName}</title>
         <meta name="description" content={description}/>
         <meta name="robots" content={robotsTags}/>
