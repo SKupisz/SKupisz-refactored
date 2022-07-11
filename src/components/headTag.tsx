46546516bc8f21ad5989db0eacf3936802117660
SKupisz <szym-ku@wp.pl>
2022-07-11 08:51:57 +0200

    Fixed an error with google search console

--- src/components/headTag.tsx
+++ src/components/headTag.tsx
@@ -15,7 +15,6 @@ const HeadTag:React.FC<HeadTagInterface> = ({siteName="Simon G. Kupisz site", de
         <title>{siteName}</title>
         <meta name="description" content={description}/>
         <meta name="robots" content={robotsTags}/>
-        <meta name="google-site-verification" content="k6qWiZWDk7r4KzuP-f2nQ1NH-i651GfErEy4BKyWluc" />
         {keywords.length > 0 ? <meta name="keywords" content={keywords}/>: <meta name="keywords" content=""/>}
         <script type="application/ld+json" data-react-helmet="true" data-rh="true">
             {
